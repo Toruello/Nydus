2a7bfbc5cd508334b6a04fa87d7526818fe14312
Toruello <egor.a.zavolokin@gmail.com>
2023-12-09 13:51:27 +0300

    fixed some typos

--- setup.sh
+++ setup.sh
@@ -19,27 +19,27 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null &&
-sudo apt-get update && 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -yqq &&
+sudo apt update && 
+sudo apt install docker.io docker-compose -yqq &&
 sudo chmod +x /usr/local/bin/docker-compose &&
 sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 
 #Configuring server parameters with user input
 
 #Desired number of peers
-read -p 'Number of peers: ' peers
+read -p '\nNumber of peers: ' peers
 sed -i "s/SET_NUMBER_OF_PEERS/$peers/g" .env
 
 #WGUI session secret, should be secure
-read -sp 'Wireguard UI session secret, not the password, put in a secure value: ' WGUISS
+read -sp '\nWireguard UI session secret, not the password, put in a secure value: ' WGUISS
 sed -i "s/SET_SESSION_SECRET/$WGUISS/g" .env
 
 #WGUI password
-read -sp 'Wireguard UI password: ' WGUIpass
+read -sp '\nWireguard UI password: ' WGUIpass
 sed -i "s/SET_PASSWORD_WGUI/$WGUIpass/g" .env
 
 #Pi-hole password
-read -sp 'Pi-hole password: ' PHpass
+read -sp '\nPi-hole password: ' PHpass
 sed -i "s/SET_PASSWORD_PIHOLE/$PHpass/g" .env
 
 # Replace the public IP placeholder in the docker-compose.yml
