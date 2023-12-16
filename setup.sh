e6ca069f2cc554a9b658053cc9ec87c9b799ac90
Toruello <egor.a.zavolokin@gmail.com>
2023-12-16 12:32:54 +0300

    Edit

--- setup.sh
+++ setup.sh
@@ -27,22 +27,22 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 #Configuring server parameters with user input
 
 #Desired number of peers
-read -p '\nNumber of peers: ' peers
+read -p '/nNumber of peers: ' peers
 sed -i "s/SET_NUMBER_OF_PEERS/$peers/g" .env
 
 #WGUI session secret, should be secure
-read -sp '\nWireguard UI session secret, not the password, put in a secure value: ' WGUISS
+read -sp '/nWireguard UI session secret, not the password, put in a secure value: ' WGUISS
 sed -i "s/SET_SESSION_SECRET/$WGUISS/g" .env
 
 #WGUI password
-read -sp '\nWireguard UI password: ' WGUIpass
+read -sp '/nWireguard UI password: ' WGUIpass
 sed -i "s/SET_PASSWORD_WGUI/$WGUIpass/g" .env
 
 #Pi-hole password
-read -sp '\nPi-hole password: ' PHpass
+read -sp '/nPi-hole password: ' PHpass
 sed -i "s/SET_PASSWORD_PIHOLE/$PHpass/g" .env
 
 # Replace the public IP placeholder in the docker-compose.yml
 sed -i "s/REPLACE_ME_WITH_YOUR_PUBLIC_IP/$(curl -s ifconfig.me)/g" docker-compose.yml
 
-docker compose up
+docker-compose up
